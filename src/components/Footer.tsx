c6c26f355589cd885325fa12bb54399d9fa78a8f
Awais Ashraf <mhawais154@gmail.com>
2025-04-20 11:03:47 +0500

    feat(ui) set basic layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{" "}
+          <a href="https://awais.com" target="_blank">
+            example.com
+          </a>
+          . Intended for learning
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+}
